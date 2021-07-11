44c89f82a950718110b70114ad05f546238b9277
Akash-Damare <81066354+Akash-Damare@users.noreply.github.com>
2021-07-11 06:23:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["lh3.googleusercontent.com", "fakestoreapi.com"],
+  },
+  env: {
+    stripe_public_key: process.env.KEY_ID,
+  },
+};
